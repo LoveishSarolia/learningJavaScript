499584cf6220128a6e6e5c6634fe85b06a27a2ce
Loveish Sarolia <46391911+LoveishSarolia@users.noreply.github.com>
2020-04-26 12:49:35 -0400

    Create whalespeak.js

---
+++ whalespeak.js
@@ -0,0 +1,19 @@
+let input = 'a whale written program';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      if (input[i] === 'e') {
+        resultArray.push('ee');
+      } else if (input[i] === 'u') {
+        resultArray.push('uu');
+      } else {
+        resultArray.push(input[i]);
+      }
+    } 
+  } 
+} 
+
+console.log(resultArray.join('').toUpperCase());
